56cc9a7f79eeb6cb6280ebfabd43e17ced3ef479
Masatoshi Nishiguchi <7563926+mnishiguchi@users.noreply.github.com>
2025-09-03 18:33:58 +0900

    Initial commit

---
+++ lib/open_jtalk_elixir/application.ex
@@ -0,0 +1,20 @@
+defmodule OpenJtalkElixir.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: OpenJtalkElixir.Worker.start_link(arg)
+      # {OpenJtalkElixir.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: OpenJtalkElixir.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
